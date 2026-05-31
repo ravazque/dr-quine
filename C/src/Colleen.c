62514baffc8b23dcd7a8477d1b092ec18db059ee
ravazque <rk.raul1306@gmail.com>
2026-05-31 12:43:40 +0200

    Colleen coments (//) & next prepared

--- C/src/Colleen.c
+++ C/src/Colleen.c
@@ -1,15 +1,17 @@
 
 #include <stdio.h>
 
-char *s = "%c#include <stdio.h>%c%cchar *s = %c%s%c;%c%cvoid print_source(char *s)%c{%c    printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main(void)%c{%c    print_source(s);%c    return (0);%c}%c";
+// Global string
+char *s = "%c#include <stdio.h>%c%c// Global string%cchar *s = %c%s%c;%c%cvoid print_source(char *s)%c{%c    printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main(void)%c{%c    // Print own source%c    print_source(s);%c    return (0);%c}%c";
 
 void print_source(char *s)
 {
-    printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+    printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main(void)
 {
+    // Print own source
     print_source(s);
     return (0);
 }
--- C/src/Sully.c
+++ C/src/Sully.c
@@ -1,9 +1,3 @@
 
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-int main(void)
-{
-    return (0);
-}
